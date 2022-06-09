634f0d29149ae48b78ebba9c291946e47067eb1d
s-nandi <saujas.nandi@gmail.com>
2022-06-08 21:21:25 -0700

    Refactor life reduction code
    - Avoid relying on player object having the 'Player' tag
    - Move life reduction code to PlayerController

--- Angled/Assets/Scripts/PlayerController.cs
+++ Angled/Assets/Scripts/PlayerController.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -32,7 +32,6 @@ public class PlayerController : MonoBehaviour
         //movement.x = Input.GetAxis("Horizontal");
         //movement.y = Input.GetAxis("Vertical");
         mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
-        if (lives <= 0) Application.Quit();
     }
 
     // Fixed Update (called 50 times a second)
@@ -43,11 +42,20 @@ public class PlayerController : MonoBehaviour
         maxSpeed = curSpeed;
         rb.velocity = new Vector2(Mathf.Lerp(0, Input.GetAxis("Horizontal") * curSpeed, 0.8f), Mathf.Lerp(0, Input.GetAxis("Vertical") * curSpeed, 0.8f));
         //rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
-        
+
         // Aiming
         Vector2 lookDir = mousePos - rb.position;
         float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90;
         rb.rotation = angle;
+    }
+
+    public void reduceLife()
+    {
+        lives--;
+        if (lives <= 0)
+        {
+            GameOver.Run();
+        }
     }
 
     public int GetLives()
